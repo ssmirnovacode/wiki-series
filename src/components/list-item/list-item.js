1d2b74d1ab68ff79aa1b29da0f0105d89a60f067
ssmirnova <svetlana.smirnova213@gmail.com>
2021-05-24 20:07:34 +0200

    Material UI enabled

--- src/components/list-item/list-item.js
+++ src/components/list-item/list-item.js
@@ -1,22 +1,58 @@
 import React, {useState, useEffect} from 'react';
 import './list-item.scss';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 const ListItem = (props) => {
 
     const {id, name, language, genres, image, rating, summary} = props.show;
 
+    const useStyles = makeStyles({
+        root: {
+          maxWidth: 345
+        },
+        media: {
+          height: 340,
+          overflow: 'hidden'
+        },
+      });
+
+    const classes = useStyles();
+
     //const descr = summary.slice(3,-3);
 
     return(
-        <article>
-            <h2>{name}</h2>
-            <img alt={name} src={image.medium} />
-            <ul>
-                {
-                    genres.map(gen => <li key={gen+id}>{gen}</li>)
-                }
-            </ul>
-        </article>
+        <Card className={classes.root}>
+            <CardActionArea>
+                <CardMedia
+                className={classes.media}
+                image={image.medium}
+                title={name}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {name}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {summary}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+{/*                 <Button size="small" color="primary">
+                Share
+                </Button> */}
+                <Button size="small" color="primary">
+                Learn More
+                </Button>
+            </CardActions>
+            </Card>
     )
 }
 
