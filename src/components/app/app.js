83bab3d4e3cbd85ebe5eac05caaac44d83d4fee8
ssmirnova <svetlana.smirnova213@gmail.com>
2021-05-26 16:57:52 +0200

    redux drafted in Header, test pending

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,21 +3,22 @@ import './app.scss';
 import Header from '../header/header';
 import Navigation from '../navigation/navigation';
 import {BrowserRouter as Router, Route } from 'react-router-dom';
-import Content from '../content/content'
+import Content from '../content/content';
+
 
 const App = () => {
 
-    const [finalQuery, setFinalQuery] = useState('');
+    //const [finalQuery, setFinalQuery] = useState('');
 
     return(
         <Router>
-            <Header  setFinalQuery={setFinalQuery}  />
+            <Header  /* setFinalQuery={setFinalQuery}  */ />
             <Navigation />
-            <Route path={'/'} component={() => <Content  finalQuery={finalQuery} />} />  {/* to be changed for SeriesPage  */}
+            <Route path={'/'} component={() => <Content  /* finalQuery={finalQuery} */ />} />  {/* to be changed for SeriesPage  */}
             
             
         </Router>
     )
-}
+};
 
 export default App;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,15 +5,15 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
-//import Badge from '@material-ui/core/Badge';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-//import MailIcon from '@material-ui/icons/Mail';
-//import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
+
+import {connect} from 'react-redux';
+import {setQuery} from '../../redux/actions';
 
 const useStyles = makeStyles((theme) => ({
     appbar: {
@@ -84,169 +84,156 @@ const useStyles = makeStyles((theme) => ({
 
 const Header = (props) =>  {
 
-        const [endpoint, setEndpoint] = React.useState('');
+  const {setQuery} = props;
 
-        const handleSubmit = (event) => {
-            event.preventDefault();
-            props.setFinalQuery(endpoint);
-            console.log('Submit completed');;
-        }
-        const classes = useStyles();
-        const [anchorEl, setAnchorEl] = React.useState(null);
-        const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-      
-        const isMenuOpen = Boolean(anchorEl);
-        const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-      
-        const handleProfileMenuOpen = (event) => {
-          setAnchorEl(event.currentTarget);
-        };
-      
-        const handleMobileMenuClose = () => {
-          setMobileMoreAnchorEl(null);
-        };
-      
-        const handleMenuClose = () => {
-          setAnchorEl(null);
-          handleMobileMenuClose();
-        };
-      
-        const handleMobileMenuOpen = (event) => {
-          setMobileMoreAnchorEl(event.currentTarget);
-        };
-      
-        const menuId = 'primary-search-account-menu';
-        const renderMenu = (
-          <Menu
-            anchorEl={anchorEl}
-            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-            id={menuId}
-            keepMounted
-            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-            open={isMenuOpen}
-            onClose={handleMenuClose}
+  const [endpoint, setEndpoint] = React.useState('');
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      setQuery(endpoint);
+      console.log('Submit completed');;
+  }
+  const classes = useStyles();
+
+  //Material UI code ---------------------------
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+  //---------------------------Material UI code 
+
+  return (
+    <div className={classes.grow}>
+      <AppBar className={classes.appbar} position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
           >
-            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-            <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-          </Menu>
-        );
-      
-        const mobileMenuId = 'primary-search-account-menu-mobile';
-        const renderMobileMenu = (
-          <Menu
-            anchorEl={mobileMoreAnchorEl}
-            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-            id={mobileMenuId}
-            keepMounted
-            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-            open={isMobileMenuOpen}
-            onClose={handleMobileMenuClose}
-          >
-            {/* <MenuItem>
-              <IconButton aria-label="show 4 new mails" color="inherit">
-                <Badge badgeContent={4} color="secondary">
-                  <MailIcon />
-                </Badge>
-              </IconButton>
-              <p>Messages</p>
-            </MenuItem>
-            <MenuItem>
-              <IconButton aria-label="show 11 new notifications" color="inherit">
-                <Badge badgeContent={11} color="secondary">
-                  <NotificationsIcon />
-                </Badge>
-              </IconButton>
-              <p>Notifications</p>
-            </MenuItem> */}
-
-            <MenuItem onClick={handleProfileMenuOpen}>
-              <IconButton
-                aria-label="account of current user"
-                aria-controls="primary-search-account-menu"
-                aria-haspopup="true"
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <p>Profile</p>
-            </MenuItem>
-          </Menu>
-        );
-      
-        return (
-          <div className={classes.grow}>
-            <AppBar className={classes.appbar} position="static">
-              <Toolbar>
-                <IconButton
-                  edge="start"
-                  className={classes.menuButton}
-                  color="inherit"
-                  aria-label="open drawer"
-                >
-                  <MenuIcon />
-                </IconButton>
-                <Typography className={classes.title} variant="h6" noWrap>
-                  WIKI-series
-                </Typography>
-                <div className={classes.search}>
-                    <div className={classes.searchIcon}>
-                        <SearchIcon />
-                    </div>
-                    <form onSubmit={(e) => handleSubmit(e)} className={classes.root}>
-                        <InputBase
-                            placeholder="Search…"
-                            classes={{
-                            root: classes.inputRoot,
-                            input: classes.inputInput,
-                            }}
-                            inputProps={{ 'aria-label': 'search' }}
-                            value={endpoint} onChange={(e) => setEndpoint(e.target.value)}
-                        />
-                    </form>
-                </div>
-                <div className={classes.grow} />
-                <div className={classes.sectionDesktop}>
-
-                  {/* <IconButton aria-label="show 4 new mails" color="inherit">
-                    <Badge badgeContent={4} color="secondary">
-                      <MailIcon />
-                    </Badge>
-                  </IconButton>
-                  <IconButton aria-label="show 17 new notifications" color="inherit">
-                    <Badge badgeContent={17} color="secondary">
-                      <NotificationsIcon />
-                    </Badge>
-                  </IconButton> */}
-
-                  <IconButton
-                    edge="end"
-                    aria-label="account of current user"
-                    aria-controls={menuId}
-                    aria-haspopup="true"
-                    onClick={handleProfileMenuOpen}
-                    color="inherit"
-                  >
-                    <AccountCircle />
-                  </IconButton>
-                </div>
-                <div className={classes.sectionMobile}>
-                  <IconButton
-                    aria-label="show more"
-                    aria-controls={mobileMenuId}
-                    aria-haspopup="true"
-                    onClick={handleMobileMenuOpen}
-                    color="inherit"
-                  >
-                    <MoreIcon />
-                  </IconButton>
-                </div>
-              </Toolbar>
-            </AppBar>
-            {renderMobileMenu}
-            {renderMenu}
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            WIKI-series
+          </Typography>
+          <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                  <SearchIcon />
+              </div>
+              <form onSubmit={(e) => handleSubmit(e)} className={classes.root}>
+                  <InputBase
+                      placeholder="Search…"
+                      classes={{
+                      root: classes.inputRoot,
+                      input: classes.inputInput,
+                      }}
+                      inputProps={{ 'aria-label': 'search' }}
+                      value={endpoint} onChange={(e) => setEndpoint(e.target.value)}
+                  />
+              </form>
           </div>
-        );
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </div>
+  );
       
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  /* items: state.items,
+  query: state.query */
+});
+
+const mapDispatchToProps = {
+  setQuery
+};
+
+export default connect(mapStateToProps)(mapDispatchToProps)(Header);
