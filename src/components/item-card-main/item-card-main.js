b8834a0aa08eec260d1f09c5c2def907bc653add
ssmirnova <svetlana.smirnova213@gmail.com>
2021-05-28 17:49:57 +0200

    ItemCardMain styled with additional features

--- src/components/item-card-main/item-card-main.js
+++ src/components/item-card-main/item-card-main.js
@@ -15,20 +15,28 @@ const ItemCardMain = (props) => {
 
     const useStyles = makeStyles({
         root: {
-          maxWidth: '18rem',
+          margin: '1rem',
+          maxWidth: '60vw',
           backgroundColor: 'rgba(0,0,0, 0.5)',
           color: 'white'
+        },
+        actionArea: {
+          display: 'grid',
+          gridTemplateColumns: '1fr 3fr'
         },
         media: {
           height: '20rem',
           overflow: 'hidden',
-          cursor: 'pointer'
+          cursor: 'pointer',
+        },
+        cardContent: {
+            display: 'grid'
         },
         descr: {
             overflow: "hidden",
             textOverflow: "ellipsis",
             display: "-webkit-box",
-            "-webkit-line-clamp": 5,
+            "-webkit-line-clamp": 6,
             "-webkit-box-orient": "vertical",
             color: 'white'
         },
@@ -40,6 +48,17 @@ const ItemCardMain = (props) => {
             fontSize: '2rem',
             marginRight: '.7rem',
             color: 'orange'
+        },
+        genres: {
+            display: 'flex',
+            gap: '1rem'
+        },
+        genre: {
+            display: 'inline-block',
+            padding: '0.3rem',
+            border: '1px solid grey',
+            borderRadius: '5px',
+            backgroundColor: 'grey'
         }
       });
 
@@ -47,13 +66,13 @@ const ItemCardMain = (props) => {
 
     return(
         <Card className={classes.root} >
-            <CardActionArea>
+            <CardActionArea className={classes.actionArea}>
                 <CardMedia
                 className={classes.media}
                 image={image ? image.medium : 'https://www.allianceplast.com/wp-content/uploads/2017/11/no-image.png'}
                 title={name} 
                 />
-                <CardContent>
+                <CardContent  className={classes.cardContent}>
                     <Typography gutterBottom variant="h5" component="h2">
                         {name}  
                     </Typography>
@@ -61,28 +80,32 @@ const ItemCardMain = (props) => {
                     <Typography className={classes.rating} gutterBottom component="h4">
                         
                         {
-                            props.page === 'shows' ? 
                             <>
-                            <StarsIcon color="action" className={classes.icon} /> 
-                            <div>
-                                {
-                                    rating.average ? rating.average : 'none'
-                                }
-                            </div></> /* : props.page === 'people' ?
-                            <div>
-                            {
-                                props.person.country ? props.person.country.name : 'Country unknown'
-                            }
-                        </div> */ : null
+                                <StarsIcon color="action" className={classes.icon} /> 
+                                <div>
+                                    {
+                                        rating.average ? rating.average : 'none'
+                                    }
+                                </div>
+                            </> 
                         }
                         
                               
                     </Typography>
                     <Typography className={classes.descr} variant="body2" color="textSecondary" component="p" 
                         dangerouslySetInnerHTML={{__html: summary ? summary : null}}/>
+                    <div className={classes.genres}>
+                        {
+                            genres.map( gen => {
+                                return(
+                                    <span className={classes.genre} key={id + gen}>{gen} </span>
+                                )
+                            })
+                        }
+                    </div>
                 </CardContent>
             </CardActionArea>
-            </Card>
+        </Card>
 
     )
 }
