61c296c1b8b10b9b95a93c1ebfcc775cd6267b78
ssmirnova <svetlana.smirnova213@gmail.com>
2021-07-15 13:38:53 +0200

    foot bug fixed

--- src/components/content/styles.js
+++ src/components/content/styles.js
@@ -2,6 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
     container: {
+        minHeight: 'calc(100vh - 30rem)',
         padding: '2rem',
         [theme.breakpoints.down('xs')] : {
             padding: '1rem'
